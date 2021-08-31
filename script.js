a4617dd741045681149a4b9c05c7b639d6d9b699
Pranjali Srivastava <srivastavapranjali603@gmail.com>
2021-08-31 21:05:33 +0530

    code added

--- script.js
+++ script.js
@@ -35,18 +35,12 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["CSE Sophmore.","Developer.", "Programmer.", "Active Learner."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
 
     // owl carousel script
     $('.carousel').owlCarousel({
